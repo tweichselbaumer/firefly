1243b33032e1cb1b4127759835828eb4f1b2172b
Thomas Weichselbaumer <thomas.weichselbaumer@hotmail.com>
2018-05-29 22:47:00 +0200

    add viewmodle and many other things

---
+++ src/FireFly/Views/CameraView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FireFly.Views
+{
+    /// <summary>
+    /// Interaction logic for CameraView.xaml
+    /// </summary>
+    public partial class CameraView : UserControl
+    {
+        public CameraView()
+        {
+            InitializeComponent();
+        }
+    }
+}
