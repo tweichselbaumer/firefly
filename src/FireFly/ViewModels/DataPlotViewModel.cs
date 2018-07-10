c04713f379fecd4dbe626c25159276f2aaa98771
Thomas Weichselbaumer <thomas.weichselbaumer@hotmail.com>
2018-07-10 18:40:57 +0200

    add gyro

--- src/FireFly/ViewModels/DataPlotViewModel.cs
+++ src/FireFly/ViewModels/DataPlotViewModel.cs
@@ -1,9 +1,28 @@
-﻿using System.Windows;
+﻿using LinkUp.Node;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace FireFly.ViewModels
 {
     public class DataPlotViewModel : AbstractViewModel
     {
+
+
+        public ObservableCollection<DataPoint> GyroXPoints
+        {
+            get { return (ObservableCollection<DataPoint>)GetValue(GyroXPointsProperty); }
+            set { SetValue(GyroXPointsProperty, value); }
+        }
+
+        public static readonly DependencyProperty GyroXPointsProperty =
+            DependencyProperty.Register("GyroXPoints", typeof(ObservableCollection<DataPoint>), typeof(DataPlotViewModel), new PropertyMetadata(new ObservableCollection<DataPoint>()));
+
+
+        private LinkUpEventLabel _EventLabel;
+
         public DataPlotViewModel(MainViewModel parent) : base(parent)
         {
         }
@@ -16,7 +35,36 @@ namespace FireFly.ViewModels
         {
             if (Parent.Node != null)
             {
+                _EventLabel = Parent.Node.GetLabelByName<LinkUpEventLabel>("firefly/test/imu_event");
+                _EventLabel.Subscribe();
+                _EventLabel.Fired += _EventLabel_Fired;
             }
+        }
+
+        private void _EventLabel_Fired(LinkUpEventLabel label, byte[] data)
+        {
+            //uint32_t timestamp;
+            //uint32_t sample;
+            //int16_t gx;
+            //int16_t gy;
+            //int16_t gz;
+            //int16_t ax;
+            //int16_t ay;
+            //int16_t az;
+            //int16_t temperature;
+            //bool cam;
+
+            double x = ((double)BitConverter.ToUInt32(data, 0)) / 1000;
+            double y = ((double)BitConverter.ToInt16(data, 8)) / 131;
+
+            Parent.SyncContext.Post(o =>
+            {
+                GyroXPoints.Add(new DataPoint(x, y));
+                if (GyroXPoints.Count > 2000)
+                {
+                    GyroXPoints.RemoveAt(0);
+                }
+            }, null);
         }
 
         private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
