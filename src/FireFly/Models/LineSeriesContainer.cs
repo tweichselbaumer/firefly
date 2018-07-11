8930b1e8dc7c046c9fd1c2a4b31f93c930669c3a
Thomas Weichselbaumer <thomas.weichselbaumer@hotmail.com>
2018-07-11 17:14:36 +0200

    add Gyro and Acc plot

---
+++ src/FireFly/Models/LineSeriesContainer.cs
@@ -0,0 +1,161 @@
+﻿using FireFly.Utilities;
+using FireFly.ViewModels;
+using OxyPlot;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace FireFly.Models
+{
+    public class LineSeriesContainer : DependencyObject
+    {
+        public static readonly DependencyProperty MajorStepYProperty =
+            DependencyProperty.Register("MajorStepY", typeof(double), typeof(LineSeriesContainer), new PropertyMetadata(10.0));
+
+        public static readonly DependencyProperty MaximumYProperty =
+            DependencyProperty.Register("MaximumY", typeof(double), typeof(LineSeriesContainer), new PropertyMetadata(10.0));
+
+        public static readonly DependencyProperty MinimumYProperty =
+            DependencyProperty.Register("MinimumY", typeof(double), typeof(LineSeriesContainer), new PropertyMetadata(10.0));
+
+        public static readonly DependencyProperty MinorStepYProperty =
+            DependencyProperty.Register("MinorStepY", typeof(double), typeof(LineSeriesContainer), new PropertyMetadata(10.0));
+
+        public static readonly DependencyProperty PointsProperty =
+            DependencyProperty.Register("Points", typeof(RangeObservableCollection<DataPoint>), typeof(LineSeriesContainer), new PropertyMetadata(new RangeObservableCollection<DataPoint>()));
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(LineSeriesContainer), new PropertyMetadata(""));
+
+        private long _AddedPoints = 0;
+        private double _MaxY = 0.0;
+        private double _MinY = 0.0;
+        private DataPlotViewModel _Parent;
+
+        private BlockingCollection<DataPoint> _BlockingCollection = new BlockingCollection<DataPoint>();
+
+        private double _TimeOffset = 0;
+
+        public LineSeriesContainer(DataPlotViewModel parent, string title)
+        {
+            Parent = parent;
+            Title = title;
+            Points = new RangeObservableCollection<DataPoint>();
+        }
+
+        public double MajorStepY
+        {
+            get { return (double)GetValue(MajorStepYProperty); }
+            set { SetValue(MajorStepYProperty, value); }
+        }
+
+        public double MaximumY
+        {
+            get { return (double)GetValue(MaximumYProperty); }
+            set { SetValue(MaximumYProperty, value); }
+        }
+
+        public double MinimumY
+        {
+            get { return (double)GetValue(MinimumYProperty); }
+            set { SetValue(MinimumYProperty, value); }
+        }
+
+        public double MinorStepY
+        {
+            get { return (double)GetValue(MinorStepYProperty); }
+            set { SetValue(MinorStepYProperty, value); }
+        }
+
+        public DataPlotViewModel Parent
+        {
+            get
+            {
+                return _Parent;
+            }
+
+            set
+            {
+                _Parent = value;
+            }
+        }
+
+        public RangeObservableCollection<DataPoint> Points
+        {
+            get { return (RangeObservableCollection<DataPoint>)GetValue(PointsProperty); }
+            set { SetValue(PointsProperty, value); }
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public void AddDataPoint(double time, double value)
+        {
+            if (_AddedPoints % 2000 == 0)
+            {
+                _TimeOffset = time;
+            }
+
+            time -= _TimeOffset;
+            _BlockingCollection.Add(new DataPoint(time, value));
+            _AddedPoints++;
+        }
+
+        public void DrawPoints()
+        {
+            int count = _BlockingCollection.Count;
+            bool clear = false;
+            List<DataPoint> points = new List<DataPoint>();
+
+
+            for (int i = 0; i < count; i++)
+            {
+                DataPoint point = _BlockingCollection.Take();
+                if (point.Y > _MaxY)
+                {
+                    _MaxY = point.Y;
+                }
+                else if (point.Y < _MinY)
+                {
+                    _MinY = point.Y;
+                }
+
+                points.Add(point);
+                if (point.X == 0)
+                {
+                    clear = true;
+                    points.Clear();
+                    _MinY = 0;
+                    _MaxY = 0;
+                }
+            }
+
+            Parent.Parent.SyncContext.Post(o =>
+            {
+                if (clear)
+                    Points.Clear();
+                Points.AddRange(points);
+                if (Points.Count > 0)
+                {
+                    double max = Math.Max(Math.Abs(_MinY), Math.Abs(_MaxY));
+                    int pow = (int)Math.Ceiling(Math.Log(max) / Math.Log(2));
+
+                    double newMaxY = Math.Pow(2, pow);
+                    if (MaximumY != newMaxY)
+                    {
+                        MaximumY = newMaxY;
+                        MinimumY = -newMaxY;
+                        MajorStepY = newMaxY / 2;
+                        MinorStepY = newMaxY / (2 * 5);
+                    }
+                }
+            }, null);
+        }
+    }
+}
--- src/FireFly/ViewModels/DataPlotViewModel.cs
+++ src/FireFly/ViewModels/DataPlotViewModel.cs
@@ -1,30 +1,112 @@
-﻿using LinkUp.Node;
-using OxyPlot;
+﻿using FireFly.Models;
+using LinkUp.Node;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Timers;
 using System.Windows;
 
 namespace FireFly.ViewModels
 {
     public class DataPlotViewModel : AbstractViewModel
     {
-
-
-        public ObservableCollection<DataPoint> GyroXPoints
-        {
-            get { return (ObservableCollection<DataPoint>)GetValue(GyroXPointsProperty); }
-            set { SetValue(GyroXPointsProperty, value); }
-        }
-
-        public static readonly DependencyProperty GyroXPointsProperty =
-            DependencyProperty.Register("GyroXPoints", typeof(ObservableCollection<DataPoint>), typeof(DataPlotViewModel), new PropertyMetadata(new ObservableCollection<DataPoint>()));
-
-
+        private LineSeriesContainer _AccX;
+        private LineSeriesContainer _AccY;
+        private LineSeriesContainer _AccZ;
         private LinkUpEventLabel _EventLabel;
+        private LineSeriesContainer _GyroX;
+        private LineSeriesContainer _GyroY;
+        private LineSeriesContainer _GyroZ;
+        private Timer _Timer;
 
         public DataPlotViewModel(MainViewModel parent) : base(parent)
         {
+            GyroX = new LineSeriesContainer(this, "Gyro X");
+            GyroY = new LineSeriesContainer(this, "Gyro Y");
+            GyroZ = new LineSeriesContainer(this, "Gyro Z");
+            AccX = new LineSeriesContainer(this, "Acc X");
+            AccY = new LineSeriesContainer(this, "Acc Y");
+            AccZ = new LineSeriesContainer(this, "Acc Z");
+
+            _Timer = new Timer(200);
+            _Timer.Elapsed += _Timer_Elapsed;
+            _Timer.Start();
+        }
+
+        public LineSeriesContainer AccX
+        {
+            get
+            {
+                return _AccX;
+            }
+
+            set
+            {
+                _AccX = value;
+            }
+        }
+
+        public LineSeriesContainer AccY
+        {
+            get
+            {
+                return _AccY;
+            }
+
+            set
+            {
+                _AccY = value;
+            }
+        }
+
+        public LineSeriesContainer AccZ
+        {
+            get
+            {
+                return _AccZ;
+            }
+
+            set
+            {
+                _AccZ = value;
+            }
+        }
+
+        public LineSeriesContainer GyroX
+        {
+            get
+            {
+                return _GyroX;
+            }
+
+            set
+            {
+                _GyroX = value;
+            }
+        }
+
+        public LineSeriesContainer GyroY
+        {
+            get
+            {
+                return _GyroY;
+            }
+
+            set
+            {
+                _GyroY = value;
+            }
+        }
+
+        public LineSeriesContainer GyroZ
+        {
+            get
+            {
+                return _GyroZ;
+            }
+
+            set
+            {
+                _GyroZ = value;
+            }
         }
 
         internal override void SettingsUpdated()
@@ -41,65 +123,12 @@ namespace FireFly.ViewModels
             }
         }
 
-        private void _EventLabel_Fired(LinkUpEventLabel label, byte[] data)
-        {
-            //uint32_t timestamp;
-            //uint32_t sample;
-            //int16_t gx;
-            //int16_t gy;
-            //int16_t gz;
-            //int16_t ax;
-            //int16_t ay;
-            //int16_t az;
-            //int16_t temperature;
-            //bool cam;
-
-            double x = ((double)BitConverter.ToUInt32(data, 0)) / 1000;
-            double y = ((double)BitConverter.ToInt16(data, 8)) / 131;
-
-            Parent.SyncContext.Post(o =>
-            {
-                GyroXPoints.Add(new DataPoint(x, y));
-                if (GyroXPoints.Count > 2000)
-                {
-                    GyroXPoints.RemoveAt(0);
-                }
-            }, null);
-        }
-
         private static void OnPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             DataPlotViewModel dpvm = (d as DataPlotViewModel);
             bool changed = false;
             switch (e.Property.Name)
             {
-                //case "Quality":
-                //    changed = cvm.Parent.SettingContainer.Settings.StreamingSettings.Quality != cvm.Quality;
-                //    cvm.Parent.SettingContainer.Settings.StreamingSettings.Quality = cvm.Quality;
-                //    try
-                //    {
-                //        if (changed)
-                //            cvm._QualityLabel.Value = (byte)cvm.Quality;
-                //    }
-                //    catch (Exception) { }
-                //    break;
-                //case "Enabled":
-                //    changed = cvm.Parent.SettingContainer.Settings.StreamingSettings.Enabled != cvm.Enabled;
-                //    cvm.Parent.SettingContainer.Settings.StreamingSettings.Enabled = cvm.Enabled;
-                //    try
-                //    {
-                //        if (changed)
-                //        {
-                //            if (cvm.Enabled)
-                //                cvm._EventLabel.Subscribe();
-                //            else
-                //                cvm._EventLabel.Unsubscribe();
-                //        }
-
-                //    }
-                //    catch (Exception) { }
-                //    break;
-
                 default:
                     break;
             }
@@ -107,6 +136,43 @@ namespace FireFly.ViewModels
             {
                 dpvm.Parent.SettingsUpdated(false);
             }
+        }
+
+        private void _EventLabel_Fired(LinkUpEventLabel label, byte[] data)
+        {
+            double gyro_scale = 16.4;
+            double acc_scale = 2048;
+            double temp_offset = 21;
+            double temp_scale = 333.8;
+
+            double time = ((double)BitConverter.ToUInt32(data, 0)) / 1000;
+
+            double gyroX = ((double)BitConverter.ToInt16(data, 8)) / gyro_scale;
+            double gyroY = ((double)BitConverter.ToInt16(data, 10)) / gyro_scale;
+            double gyroZ = ((double)BitConverter.ToInt16(data, 12)) / gyro_scale;
+
+            double accX = ((double)BitConverter.ToInt16(data, 14)) / acc_scale;
+            double accY = ((double)BitConverter.ToInt16(data, 16)) / acc_scale;
+            double accZ = ((double)BitConverter.ToInt16(data, 18)) / acc_scale;
+
+            double temperatur = ((double)BitConverter.ToInt16(data, 20)) / temp_scale + temp_offset;
+
+            GyroX.AddDataPoint(time, gyroX);
+            GyroY.AddDataPoint(time, gyroY);
+            GyroZ.AddDataPoint(time, gyroZ);
+            AccX.AddDataPoint(time, accX);
+            AccY.AddDataPoint(time, accY);
+            AccZ.AddDataPoint(time, accZ);
+        }
+
+        private void _Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            GyroX.DrawPoints();
+            GyroY.DrawPoints();
+            GyroZ.DrawPoints();
+            AccX.DrawPoints();
+            AccY.DrawPoints();
+            AccZ.DrawPoints();
         }
     }
 }
