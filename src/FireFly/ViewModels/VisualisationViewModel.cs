74798a29b865e87638890e40d440e59f65c8f2ae
Thomas Weichselbaumer <thomas.weichselbaumer@hotmail.com>
2018-11-26 14:53:09 +0100

    add fps

--- src/FireFly/ViewModels/VisualisationViewModel.cs
+++ src/FireFly/ViewModels/VisualisationViewModel.cs
@@ -1,19 +1,37 @@
 ﻿using FireFly.Proxy;
+using FireFly.Utilities;
 using FireFly.VI.SLAM;
 using FireFly.VI.SLAM.Visualisation;
 using System.Collections.Generic;
+using System.Timers;
+using System.Windows;
 
 namespace FireFly.ViewModels
 {
     public class VisualisationViewModel : AbstractViewModel, IProxyEventSubscriber
     {
+        public static readonly DependencyProperty FPSProperty =
+            DependencyProperty.Register("FPS", typeof(int), typeof(VisualisationViewModel), new PropertyMetadata(0));
+
+        private FPSCounter _FPSCounter = new FPSCounter();
         private SlamModel3D _SlamModel3D;
+
+        private Timer _Timer;
 
         public VisualisationViewModel(MainViewModel parent) : base(parent)
         {
             _SlamModel3D = new SlamModel3D(Parent.SyncContext);
             _SlamModel3D.AddNewFrame(new Frame(0, 0, 0, 0, 1, 0, 0, 0, 1));
             Parent.IOProxy.Subscribe(this, ProxyEventType.SlamMapEvent);
+            _Timer = new Timer(300);
+            _Timer.Elapsed += _Timer_Elapsed;
+            _Timer.Start();
+        }
+
+        public int FPS
+        {
+            get { return (int)GetValue(FPSProperty); }
+            set { SetValue(FPSProperty, value); }
         }
 
         public SlamModel3D SlamModel3D
@@ -33,6 +51,7 @@ namespace FireFly.ViewModels
                 if (data.PublishType == SlamPublishType.Frame)
                 {
                     SlamModel3D.AddNewFrame(data.Frame);
+                    _FPSCounter.CountFrame();
                 }
                 else if (data.PublishType == SlamPublishType.KeyframeWithPoints)
                 {
@@ -43,6 +62,14 @@ namespace FireFly.ViewModels
                     SlamModel3D.Reset();
                 }
             }
+        }
+
+        private void _Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Parent.SyncContext.Post(o =>
+            {
+                FPS = (int)_FPSCounter.FramesPerSecond;
+            }, null);
         }
     }
 }
